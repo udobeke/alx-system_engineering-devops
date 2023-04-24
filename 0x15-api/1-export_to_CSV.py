a7a87e1db32058268cf57cc335f50ac56fff3f51
Peace Chinagwam <111175597+udobeke@users.noreply.github.com>
2023-04-24 15:32:20 +0100

    Update 1-export_to_CSV.py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,27 +1,65 @@
 #!/usr/bin/python3
 """Accessing a REST API for todo lists of employees and exporting data in CSV format"""
 
-
 import csv
 import requests
 import sys
 
-if __name__ == '__main__':
+
+def main():
+    """Main function to fetch and display employee TODO list progress and export data in CSV"""
+
+    if len(sys.argv) != 2:
+        print("Usage: python3 todo_list.py EMPLOYEE_ID")
+        sys.exit(1)
+
     employee_id = sys.argv[1]
     base_url = "https://jsonplaceholder.typicode.com/users"
-    url = f"{base_url}/{employee_id}"
+    url = "{}/{}".format(base_url, employee_id)
 
-    response = requests.get(url)
-    employee_name = response.json().get('username')
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        employee_name = response.json().get('name')
+    except requests.exceptions.RequestException as e:
+        print("An error occurred while accessing the API. Error: {}".format(e))
+        sys.exit(1)
 
-    todo_url = f"{url}/todos"
-    response = requests.get(todo_url)
-    tasks = response.json()
+    todo_url = "{}/todos".format(url)
 
-    with open(f"{employee_id}.csv", mode='w', newline='') as file:
-        writer = csv.writer(file)
+    try:
+        response = requests.get(todo_url)
+        response.raise_for_status()
+        tasks = response.json()
+        done_tasks = [task for task in tasks if task['completed']]
+    except requests.exceptions.RequestException as e:
+        print("An error occurred while accessing the employee's TODO list. Error: {}".format(e))
+        sys.exit(1)
+
+    num_done_tasks = len(done_tasks)
+    num_total_tasks = len(tasks)
+
+    print("Employee {} is done with tasks({}/{}):".format(employee_name, num_done_tasks, num_total_tasks))
+
+    for task in done_tasks:
+        print("\t{}".format(task['title']))
+
+    # Export data in CSV format
+    csv_filename = "{}.csv".format(employee_id)
+    with open(csv_filename, 'w', newline='') as csvfile:
+        fieldnames = ['USER_ID', 'USERNAME', 'TASK_COMPLETED_STATUS', 'TASK_TITLE']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
         for task in tasks:
-            task_status = 'True' if task.get('completed') else 'False'
-            task_title = task.get('title')
-            writer.writerow([employee_id, employee_name, task_status, task_title])
-            print(f'"{employee_id}","{employee_name}","{task_status}","{task_title}"')
+            writer.writerow({
+                'USER_ID': employee_id,
+                'USERNAME': employee_name,
+                'TASK_COMPLETED_STATUS': task['completed'],
+                'TASK_TITLE': task['title']
+            })
+
+    print("Data exported to {} in CSV format.".format(csv_filename))
+
+
+if __name__ == '__main__':
+    main()
