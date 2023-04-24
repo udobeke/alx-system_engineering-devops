3a86014f6c622056ebd597d8e3d99a24607d725c
Peace Chinagwam <111175597+udobeke@users.noreply.github.com>
2023-04-24 19:47:59 +0100

    Update 1-export_to_CSV.py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -6,38 +6,32 @@ import csv
 import requests
 import sys
 
-if len(sys.argv) != 2:
-    print("Usage: python3 export_to_CSV.py USER_ID")
-    sys.exit(1)
+if __name__ == '__main__':
+    import requests
+    from sys import argv
+    emp_id = argv[1]
+    file_name = emp_id + '.csv'
+    total_todos = 0
+    done_todos = 0
+    done_todos_titles = []
 
-user_id = sys.argv[1]
-url = "https://jsonplaceholder.typicode.com"
-user_url = url + "/users/" + user_id
-tasks_url = url + "/todos?userId=" + user_id
+    res = requests.get(
+                   'https://jsonplaceholder.typicode.com/users/' +
+                   emp_id)
+    emp_name = res.json().get('username')
 
-# Fetch user data
-user_data = requests.get(user_url).json()
-username = user_data["username"]
+    res = requests.get(
+                   'https://jsonplaceholder.typicode.com/users/' +
+                   emp_id + '/todos')
+    emp_todos = res.json()
 
-# Fetch tasks data
-tasks_data = requests.get(tasks_url).json()
-
-# Write data to CSV file
-file_name = user_id + ".csv"
-with open(file_name, "w", newline="") as csvfile:
-    fieldnames = ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"]
-    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-    writer.writeheader()
-    for task in tasks_data:
-        completed_status = "True" if task["completed"] else "False"
-        writer.writerow(
-            {
-                "USER_ID": user_id,
-                "USERNAME": username,
-                "TASK_COMPLETED_STATUS": completed_status,
-                "TASK_TITLE": task["title"],
-            }
-        )
-
-print("Data has been exported to", file_name, "successfully!")
-
+    with open(file_name, 'w') as csvfile:
+        for item in emp_todos:
+            total_todos += 1
+            csvfile.write(
+                    '"{}","{}","{}","{}"\n'.format(
+                                            item.get('userId'),
+                                            emp_name,
+                                            item.get('completed'),
+                                            item.get('title')
+                                            ))
