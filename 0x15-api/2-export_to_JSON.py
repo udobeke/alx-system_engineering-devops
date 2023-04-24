031ff1c339c6bff833dfe43a96c9accaa16585ce
Peace Chinagwam <111175597+udobeke@users.noreply.github.com>
2023-04-24 20:24:43 +0100

    Update 2-export_to_JSON.py

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -2,16 +2,15 @@
 
 """This script uses the `json` module to write the tasks data"""
 
-
-import json
-import sys
-import requests
 import csv
-
+import json
+import requests
+import sys
 
 if __name__ == '__main__':
-    import requests
     import json
+    import requests
+    import sys
     from sys import argv
 
     emp_id = argv[1]
@@ -20,12 +19,10 @@ if __name__ == '__main__':
     done_todos = 0
     done_todo_titles = []
 
-    res = requests.get('https://jsonplaceholder.typicode.com/users/' +
-                       emp_id)
+    res = requests.get('https://jsonplaceholder.typicode.com/users/' + emp_id)
     emp_username = res.json().get('username')
 
-    res = requests.get('https://jsonplaceholder.typicode.com/users/' +
-                       emp_id + '/todos')
+    res = requests.get('https://jsonplaceholder.typicode.com/users/' + emp_id + '/todos')
     emp_todos = res.json()
 
     records = {str(emp_id): []}
@@ -33,8 +30,8 @@ if __name__ == '__main__':
     for item in emp_todos:
         total_todos += 1
         records[str(emp_id)].append({"task": item.get('title'),
-                                     "completed": item.get("completed"),
-                                     "username": emp_username})
+                                      "completed": item.get("completed"),
+                                      "username": emp_username})
 
     with open(file_name, 'w') as jsonfile:
         json.dump(records, jsonfile)
