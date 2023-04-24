46ab2144bedeba450c06fbe67a6c7eb7c147b197
Peace Chinagwam <111175597+udobeke@users.noreply.github.com>
2023-04-24 15:41:25 +0100

    Update 1-export_to_CSV.py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -58,6 +58,7 @@ def main():
                 'TASK_TITLE': task['title']
             })
 
+    print("Number of tasks in CSV: {}".format(num_total_tasks))
     print("Data exported to {} in CSV format.".format(csv_filename))
 
 
