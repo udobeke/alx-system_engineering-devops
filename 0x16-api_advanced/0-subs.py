52f9269ef873841a14a6100bcf19d9f7280a9944
Peace Chinagwam <111175597+udobeke@users.noreply.github.com>
2023-05-09 17:07:23 +0100

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 """ a script to retrieve the no of subs on a subreddit"""
 
 import requests
@@ -8,7 +7,6 @@ def number_of_subscribers(subreddit):
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "My Agent"}
     response = requests.get(url, headers=headers, allow_redirects=False)
-
     if response.status_code == 200:
         data = response.json()
         no_subs = data.get("data", {}).get("subscribers", 0)
